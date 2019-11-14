c94ec209c23df6de3be5c4f47aac5b1db30eb761
massimorenson <57722213+massimorenson@users.noreply.github.com>
2019-11-14 14:48:05 +0100

    datasets ready

--- image_px.py
+++ image_px.py
@@ -13,7 +13,7 @@ def img2bin(img_px):
     return bin_array
 
 def generate_training_data():
-    img_bin_data_array = np.int_([])
+    img_bin_data_array = np.empty((0,784), int) # np.int_([])
     print(img_bin_data_array.shape)
     for i in range(10):
         for j in range(11):
@@ -21,18 +21,41 @@ def generate_training_data():
             image = Image.open(path,'r')
             img_px = np.array(image)
             img_bin = img2bin(img_px)
-            img_bin_data_array = np.append(img_bin_data_array,img_bin,axis=0) #TODO make new row instead of appending on the same one
-     return img_bin_data_array
+            img_bin_data_array = np.append(img_bin_data_array,np.array([img_bin]),axis=0)
+    return img_bin_data_array
 
 def generate_output_data():
-    #TODO 
     # [0 0 0 0 0 0 0 0 0 0]
     # = indexen zullen de cijfers worden die voorpeld worden
     # [0 1 2 3 4 5 6 7 8 9]
-    return ''
+    zero = np.array([1,0,0,0,0,0,0,0,0,0])
+    one =  np.array([0,1,0,0,0,0,0,0,0,0])
+    two =  np.array([0,0,1,0,0,0,0,0,0,0])
+    three =np.array([0,0,0,1,0,0,0,0,0,0])
+    four = np.array([0,0,0,0,1,0,0,0,0,0])
+    five = np.array([0,0,0,0,0,1,0,0,0,0])
+    six =  np.array([0,0,0,0,0,0,1,0,0,0])
+    seven =np.array([0,0,0,0,0,0,0,1,0,0])
+    eight =np.array([0,0,0,0,0,0,0,0,1,0])
+    nine = np.array([0,0,0,0,0,0,0,0,0,1])
+
+    digits = [zero, one, two, three, four, five, six, seven, eight, nine]
+
+    sol_data = np.empty((0,10), int)
+    for digit in digits:
+        for i in range(11):
+            sol_data = np.append(sol_data, np.array([digit]), axis=0)
+    return sol_data
 
 if __name__ == "__main__":
-    print('generating...')
+    print('generating... fase1')
     training_data=generate_training_data()
     print(training_data.shape)
+    print('generating... fase2')
+    output_data = generate_output_data()
+    print(output_data.shape)
+
+
+
+
 
